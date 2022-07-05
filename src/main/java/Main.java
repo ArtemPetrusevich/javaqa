e6d1b7bb6c86962117c2d5ab3440b64b5624d463
artem <goldkuka@gmail.com>
2022-07-05 23:34:28 +0700

    initcomm

---
+++ src/main/java/Main.java
@@ -0,0 +1,9 @@
+public class Main {
+    public static void main(String[] args) {
+        BonusService service = new BonusService();
+                long bonus = service.calculate(1000, false);
+        System.out.println("Your bonus:"+ bonus);
+
+
+    }
+}
